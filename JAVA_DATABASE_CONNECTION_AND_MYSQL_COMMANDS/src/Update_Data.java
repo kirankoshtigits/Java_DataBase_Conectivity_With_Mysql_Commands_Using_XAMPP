2d2d395ebf27050b94cfd3f6d39bed927d3875df
student <student@MDB-PC-01>
2018-10-01 17:11:31 +0530

    Update Data in the Database

---
+++ JAVA_DATABASE_CONNECTION_AND_MYSQL_COMMANDS/src/Update_Data.java
@@ -0,0 +1,39 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+public class Update_Data {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String JDBC_DRIVER="com.mysql.jdbc.Driver";
+		String DB_URL="jdbc:mysql://localhost/java_database";
+		String DB_USER="root";
+		String DB_Pass="";
+		Connection conn=null;
+		Statement stm=null;
+
+		String query="update student SET Name='karan' WHERE id=1"; 
+		
+		try
+		{
+			Class.forName(JDBC_DRIVER);
+			conn = DriverManager.getConnection(DB_URL,DB_USER,DB_Pass);
+			
+			stm=conn.createStatement();
+			
+			stm.execute(query);
+			
+			System.out.println("\n\nData Is Update Successfull.......!");
+	
+			stm.close();
+			conn.close();
+		}
+		catch(Exception e) 
+		{
+			System.out.println("Error...Connection is faild:"+e);
+		}
+
+	}
+
+}
